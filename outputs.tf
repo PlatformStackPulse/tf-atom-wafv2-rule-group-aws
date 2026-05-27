3b0130a0408d19e153e91716e6db84a19c8d37fe
PE Stack Pulse <Ali.Sarshogh@outlook.com>
2026-05-27 09:34:48 +0100

    feat: implement tf-atom-wafv2-rule-group-aws (#3)
    * feat: implement tf-atom-wafv2-rule-group-aws resource
    
    * docs: update terraform-docs
    
    ---------
    
    Co-authored-by: Harry S <harry@LDN-LAP-997576.corp.com>

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,18 @@ output "enabled" {
   description = "Whether the module is enabled."
   value       = local.enabled
 }
+
+output "id" {
+  description = "The ID of the rule group."
+  value       = try(aws_wafv2_rule_group.default[0].id, "")
+}
+
+output "arn" {
+  description = "The ARN of the rule group."
+  value       = try(aws_wafv2_rule_group.default[0].arn, "")
+}
+
+output "capacity" {
+  description = "The WCU capacity of the rule group."
+  value       = try(aws_wafv2_rule_group.default[0].capacity, 0)
+}
